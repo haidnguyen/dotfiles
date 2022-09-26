eb507a9f782b8fc397d010d902061c31ff0abd7f
haidnguyen <dinhhai281@gmail.com>
2022-09-26 20:07:56 +0700

    add cmp

--- nvim/lua/user/plugins.lua
+++ nvim/lua/user/plugins.lua
@@ -50,10 +50,20 @@ return packer.startup(function(use)
 
 	-- ALL PLUGINS HERE
 	use 'folke/tokyonight.nvim'
+	
+	use 'neovim/nvim-lspconfig'
 
-
-
-
+	-- cmp plugins
+	use 'hrsh7th/cmp-nvim-lsp'
+	use 'hrsh7th/cmp-buffer'
+	use 'hrsh7th/cmp-path'
+	use 'hrsh7th/cmp-cmdline'
+	use 'hrsh7th/nvim-cmp'
+	
+	-- snippets
+	use 'L3MON4D3/LuaSnip'
+	use 'saadparwaiz1/cmp_luasnip'
+	use 'rafamadriz/friendly-snippets'
 
 
 
