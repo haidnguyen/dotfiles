33538e522b9deedae23a6e8994c23a4d367d60e9
Nguyen Dinh Hai <haind@anymindgroup.com>
2022-09-26 14:45:29 +0700

    init config

---
+++ nvim/lua/user/options.lua
@@ -0,0 +1,14 @@
+vim.opt.number = true
+vim.opt.cursorline = true
+vim.opt.mouse = 'a'
+vim.opt.backup = false
+vim.opt.cmdheight = 2
+vim.opt.fileencoding = 'utf-8-'
+vim.opt.hlsearch = true
+vim.opt.ignorecase = true
+vim.opt.smartcase = true
+vim.opt.splitbelow = true
+vim.opt.splitright = true
+vim.opt.tabstop = 2
+vim.opt.scrolloff = 10
+
